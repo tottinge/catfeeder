00be95e4f5c3593a400f46acd515921ad42242b1
Tim Ottinger <tottinge@industriallogic.com>
2020-07-09 16:59:01 -0500

    ignore venv in flake8

--- runner.py
+++ runner.py
@@ -1,5 +1,7 @@
-from bottle import route, run, static_file
 import os
+
+from bottle import route, run, static_file
+
 
 @route('/')
 def catFeeder():
@@ -9,8 +11,10 @@ def catFeeder():
 def get_ipaddr():
     import socket
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.connect( ('google.com',1) )
-        host,_ =  s.getsockname()
+        s.connect(('google.com', 1))
+        host, _ = s.getsockname()
         return host
 
-run(host=get_ipaddr(), port='8001', reloader=True)
+
+if __name__ == '__main__':
+    run(host=get_ipaddr(), port='8001', reloader=True)
